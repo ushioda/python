16e315ca198044346bb44cdb3979cc774f75a604
ushioda <yuu1023@gmaill.com>
2013-12-27 12:49:09 +0900

    some changes

--- get_gdp.py
+++ get_gdp.py
@@ -6,8 +6,14 @@ from bs4 import BeautifulSoup
  
 http = urllib3.PoolManager()
 r = http.request('GET', 'http://en.wikipedia.org/wiki/List_of_countries_by_GDP_(PPP)_per_capita')
+
 s = r.data
 
 soup = BeautifulSoup(s)
-table = soup.findall("tr")
+table = soup.findall("table",attrs={ "class" : "table-horizontal-line"})
 print table
+
+# print soup.prettify()
+
+# for a in soup("a"):
+#   print a.contents
