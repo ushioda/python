55c065bd8cd29a6d9ede3bd9ced95939ac8f5107
ushioda <yuu1023@gmaill.com>
2013-12-27 15:33:12 +0900

    fin

--- get_gdp.py
+++ get_gdp.py
@@ -5,14 +5,13 @@ import urllib3
 from bs4 import BeautifulSoup
  
 http = urllib3.PoolManager()
-r = http.request('GET', 'http://aoimorirailway.com/jikoku/zikoku_2_018.html')
+r = http.request('GET', 'http://cse.naro.affrc.go.jp/takezawa/r-tips/r/20.html')
 s = r.data
 
 soup = BeautifulSoup(s)
 
 table = soup.findAll("table")
 print table
-
 
 # row = table.findAll("tr")
 # print row
